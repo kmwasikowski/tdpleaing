295c6168b3f9bcbe66a5ecfd82bf95e9301c061e
Kristyn Wasikowski <Kxw2039@rit.edu>
2016-11-02 18:11:36 -0400

    first attempt

---
+++ week 2 - conditions/week2.rb
@@ -0,0 +1,99 @@
+
+#Create the following statements
+
+•If
+
+
+If cookies .has(Chocolate)
+
+Puts “Kristyn is excited for cookies”
+
+•If else
+
+
+If cookies .has(Chocolate)
+
+Puts “Kristyn is excited for cookies”
+
+Else
+
+Puts “Kristyn doesn’t want the cookies”
+
+•If elsif else
+
+
+If cookies .has(Chocolate)
+
+Puts “Kristyn is excited for cookies”
+
+Elseif cookies .has(Peanutbutter)
+
+Puts “Kristyn wants the cookies”
+
+Else
+
+Puts “Kristyn doesn’t want the cookies”
+
+•Opprand (Same thing as Ternary?)
+
+
+If cookies .has(chocolate) ? puts “Kristyn is excited for cookies” “ puts “Kristyn doesn't want the cookies”
+
+•Case
+
+
+Case Cookies
+
+     When “Chocolate”
+
+    Puts “Yummy”
+
+    When “Peanutbutter”
+
+    Puts “Delicious
+
+     When Oatmeal
+
+    Puts “Gross”
+
+    Else
+
+    Puts “Haven’t tried”
+
+
+
+
+
+
+
+If you want to push yourself try the following
+
+•An if else statement with an if else statement inside of these
+
+
+◦Try to hit all of the conditions there should be 4 possible outcomes
+
+
+•Case statement with multiple values on a when line
+
+
+Case Cookies
+
+     When “Chocolate” or “Peanutbutter”
+
+    Puts “Yummy”
+
+    When “Oatmeal”
+
+    Puts “Gross”
+
+    Else
+
+    Puts “Haven’t tried”
+
+
+
+
+•Setting a variable using an ternary
+
+
